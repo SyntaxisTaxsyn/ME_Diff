72287b12ab4db6662c0f4782af218ac140e73c7b
SyntaxisTaxsyn <66139375+SyntaxisTaxsyn@users.noreply.github.com>
2021-01-25 11:01:51 +0000

    Gauge and graph - All objects unit tested

--- ME_Diff_UnitTesting/UnitTest1.vb
+++ ME_Diff_UnitTesting/UnitTest1.vb
@@ -2252,4 +2252,36 @@ End Class
 
     End Sub
 
+    <TestMethod()> Public Sub _0502ScaleTest()
+
+        ' main tests
+        Dim fcomp As New FileCompare
+        Dim P As String = "05Gauge and graph"
+        Dim C As String = "02Scale"
+        Dim I As String = "0502"
+        Dim N As String = "Scale"
+        Dim T As String = "main"
+        Dim tdfstr As String = GetPathToFileCSV(P, C, I, N & T) ' Added "& T" to N param as this test has multiple subtypes
+        Dim Lfile As String = GetPathToFileXML(P, C, I, N, "L", T)
+        Dim Rfile As String = GetPathToFileXML(P, C, I, N, "R", T)
+        Dim ThisTestCase As New TestCases(tdfstr)
+        Dim strlst As List(Of String)
+        Dim Result(50) As TestResult
+
+        For a = 0 To Result.Length - 1
+            Result(a) = TestResult.Fail
+        Next
+
+        fcomp.SetFilePaths(Lfile, Rfile)
+        strlst = fcomp.CompareFiles(FileCompare.UnitTestType.FixedFile)
+        For a = 0 To ThisTestCase.TestList.Count - 1
+            Result(a) = CheckTestCriteria(ThisTestCase.TestList.Item(a), strlst)
+            Assert.IsTrue(Result(a), "Failed at index " & a &
+                          ",Property - " & ThisTestCase.TestList.Item(a).sProperty &
+                          ",Left - " & ThisTestCase.TestList.Item(a).sLeftval &
+                          ",Right - " & ThisTestCase.TestList.Item(a).sRightval)
+        Next
+
+    End Sub
+
 End Class
